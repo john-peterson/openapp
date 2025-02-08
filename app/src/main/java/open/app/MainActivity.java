b77906353009a11d4bba1bf962cfdd8a0452ec18
John Sebastian Peterson <jpeterson57@gmail.com>
2025-02-08 12:32:01 +1100

    tabs

--- app/src/main/java/open/app/MainActivity.java
+++ app/src/main/java/open/app/MainActivity.java
@@ -6,22 +6,22 @@ import android.os.Bundle;
 import open.app.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
+	private ActivityMainBinding binding;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        // Inflate and get instance of binding
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+		// Inflate and get instance of binding
+		binding = ActivityMainBinding.inflate(getLayoutInflater());
 
-        // set content view to binding's root
-        setContentView(binding.getRoot());
-    }
-    
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        this.binding = null;
-    }
+		// set content view to binding's root
+		setContentView(binding.getRoot());
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		this.binding = null;
+	}
 }
