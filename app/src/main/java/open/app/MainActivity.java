37de7accfc622054bfc55a06b69a70e7ceec1b6a
John Sebastian Peterson <jpeterson57@gmail.com>
2025-02-08 12:46:10 +1100

    keyboard intercept

--- app/src/main/java/open/app/MainActivity.java
+++ app/src/main/java/open/app/MainActivity.java
@@ -2,11 +2,22 @@
 package open.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
 import open.app.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 	private ActivityMainBinding binding;
+
+	@Override
+	public boolean onKeyDown(int key, KeyEvent event) {
+		// eMappedToEscape() || keyCode != KeyEvent.KEYCODE_BACK))
+		Toast.makeText(this, "roast"+ key + " ",Toast.LENGTH_SHORT).show();
+		return super.onKeyDown(key, event);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
