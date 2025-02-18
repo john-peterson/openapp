de56d87825c3471b628b45b4b1d0aba5f475bf15
John Sebastian Peterson <jpeterson57@gmail.com>
2025-02-19 01:30:45 +1100

    add https://github.com/extmkv/CircularViewPager

--- app/src/main/java/open/app/MainActivity.kt
+++ app/src/main/java/open/app/MainActivity.kt
@@ -1,29 +1,19 @@
+package com.extmkv.circularviewpager.example
 
-package open.app
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import open.app.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-public class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
-    
-    private val binding: ActivityMainBinding
-      get() = checkNotNull(_binding) { "Activity has been destroyed" }
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        // Inflate and get instance of binding
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-
-        // set content view to binding's root
-        setContentView(binding.root)
-    }
-    
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+        val circularAdapter = CircularPagerAdapter(supportFragmentManager, lifecycle)
+        vwpHome.apply {
+            adapter = circularAdapter
+            setCurrentItem(circularAdapter.getCenterPage(), false)
+        }
     }
 }
