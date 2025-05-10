9f122da12d42f61b115ab91d0b5491564fbe4267
John Sebastian Peterson <jpeterson57@gmail.com>
2025-05-11 01:41:50 +0300

    log

--- app/src/main/java/open/app/MainActivity.java
+++ app/src/main/java/open/app/MainActivity.java
@@ -3,13 +3,23 @@ package open.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import java.util.logging.Logger;
 import open.app.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 	private ActivityMainBinding binding;
+	String tag = "mytag";
+	private static final Logger LOGGER = Logger.getLogger(MainActivity.class.getName());
+
+	void log() {
+        LOGGER.warning("123abc java utilities log");
+				Log.e(tag, "123abc android utilities log");
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		log();
 		super.onCreate(savedInstanceState);
 
 		// Inflate and get instance of binding
