537b6af07e557712e56bf00cec474a8a994338ad
John Peterson <jpeterson57@gmail.com>
2026-01-14 00:48:01 +1100

    tabs

--- app/src/main/java/open/app/MainActivity.kt
+++ app/src/main/java/open/app/MainActivity.kt
@@ -7,23 +7,23 @@ import open.app.databinding.ActivityMainBinding
 
 public class MainActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
-    
-    private val binding: ActivityMainBinding
-      get() = checkNotNull(_binding) { "Activity has been destroyed" }
-    
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+	private var _binding: ActivityMainBinding? = null
+	
+	private val binding: ActivityMainBinding
+	  get() = checkNotNull(_binding) { "Activity has been destroyed" }
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
 
-        // Inflate and get instance of binding
-        _binding = ActivityMainBinding.inflate(layoutInflater)
+		// Inflate and get instance of binding
+		_binding = ActivityMainBinding.inflate(layoutInflater)
 
-        // set content view to binding's root
-        setContentView(binding.root)
-    }
-    
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-    }
+		// set content view to binding's root
+		setContentView(binding.root)
+	}
+	
+	override fun onDestroy() {
+		super.onDestroy()
+		_binding = null
+	}
 }
