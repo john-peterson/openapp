963ff0661eac681f1a2e38fb9e00ecc05d15bbf7
John Peterson <jpeterson57@gmail.com>
2026-01-15 11:31:14 +1100

    settings

--- app/src/main/java/open/app/MainActivity.kt
+++ app/src/main/java/open/app/MainActivity.kt
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import open.app.databinding.ActivityMainBinding
 
+import android.util.Log
+// import android.content.Context
+
+import android.content.ContentResolver
+import android.provider.Settings
+
 public class MainActivity : AppCompatActivity() {
 
 	private var _binding: ActivityMainBinding? = null
@@ -12,7 +18,25 @@ public class MainActivity : AppCompatActivity() {
 	private val binding: ActivityMainBinding
 	  get() = checkNotNull(_binding) { "Activity has been destroyed" }
 	
+		val tag = "mytag"
+
+		fun bat(){
+			// val contentResolver: ContentResolver = context.contentResolver
+			val contentResolver: ContentResolver = getContentResolver()
+
+			val c = Settings.Global.getString(
+				contentResolver,
+				"battery_saver_constants"
+				// BatterySaverSecureSettings.BATTERY_SAVER_CONSTANTS
+			)
+			Log.e(tag, ""+c)
+
+		}
+
+	
 	override fun onCreate(savedInstanceState: Bundle?) {
+		// Log.e(tag, "")
+		bat();
 		super.onCreate(savedInstanceState)
 
 		// Inflate and get instance of binding
