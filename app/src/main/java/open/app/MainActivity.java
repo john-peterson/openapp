5d7bfec506a502b1fff8b8d8adad75a969f9cb9f
John Peterson <jpeterson57@gmail.com>
2026-05-11 19:22:00 -0300

    json

--- app/src/main/java/open/app/MainActivity.java
+++ app/src/main/java/open/app/MainActivity.java
@@ -6,17 +6,70 @@ import android.os.Bundle;
 import android.util.Log;
 import open.app.databinding.ActivityMainBinding;
 
+import android.util.JsonWriter;
+import java.util.List;
+// import java.io.PrintWriter;
+import java.io.StringWriter;
+
+class myWriter {
+	StringWriter sw;
+	public myWriter(StringWriter s) {
+		sw = s;
+	}
+	void value(String s) {
+		sw.append(s);
+	}
+}
+
 public class MainActivity extends AppCompatActivity {
 private ActivityMainBinding binding;
 String tag = "mytag";
 
+static StringWriter sw;
+// static JsonWriter out;
+static myWriter out;
+
 void log() {
-	Log.e(tag, "");
+	Log.e(tag, "json test");
+}
+
+
+void json(){
+	sw = new StringWriter();
+	// out = new JsonWriter(sw);
+	// out.setIndent("  ");
+	// out.setIndent("	");
+
+	try {
+		jsonTest();
+		// out.endObject();
+		Log.e(tag, sw.toString());
+	} catch (Exception e) {
+		Log.e(tag, e.toString());
+	}
+}
+
+// # [ array of values
+// # { object of key value pair
+static void jsonTest() throws Exception {
+	out.beginArray();
+	out.value("123");
+	out.endArray();
+	
+	// out.beginObject();
+	// out.name("on").value(0);
+	// out.endObject();
+
+	// out.beginObject();
+	// out.name("123");
+	// out.name("on").value(0);
+	
 }
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
 	log();
+	json();
 	super.onCreate(savedInstanceState);
 
 	// Inflate and get instance of binding
